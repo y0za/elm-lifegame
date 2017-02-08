36fdbbea7176b095f12567115af7815c2f6203b0
y0za <yoza.is12s@gmail.com>
2017-02-09 00:57:06 +0900

    Add board whose cell can be toggled

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,33 +1,55 @@
 module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Html.Events exposing (onClick)
+import List
 
 
 -- APP
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
-type alias Model = Int
+type alias Model =
+  { board : List (List Bool)
+  }
 
-model : number
-model = 0
+model : Model
+model =
+  { board = List.repeat 8 <| List.repeat 8 False
+  }
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg = ToggleCell Int Int
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    NoOp -> model
-    Increment -> model + 1
+    ToggleCell y x ->
+      { model | board =
+        model.board
+          |> List.indexedMap
+            (\index row ->
+              if index == y then
+                toggleCellInRow row x
+              else
+                row
+            )
+      }
+
+toggleCellInRow : List Bool -> Int -> List Bool
+toggleCellInRow row target =
+  let
+    toggle index state =
+      if index == target then
+        not state
+      else
+        state
+  in
+    List.indexedMap toggle row
 
 
 -- VIEW
@@ -36,20 +58,27 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
+    boardView model.board
   ]
+
+boardView : List (List Bool) -> Html Msg
+boardView board =
+  Html.table [ class "board" ] <|
+    List.indexedMap
+      (\y row ->
+        tr [] (
+          row |> List.indexedMap
+            (\x state -> td [ class <| stateClass state, onClick <| ToggleCell y x ] [])
+        )
+      )
+      board
+
+stateClass : Bool -> String
+stateClass state =
+  if state then
+    "alive"
+  else
+    "dead"
 
 
 -- CSS STYLES
