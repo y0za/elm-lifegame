3329c6f91045c2e19aab50baf75596db9a5f2f16
y0za <yoza.is12s@gmail.com>
2017-02-09 21:15:41 +0900

    Add Core.Board and its helper functions

--- src/elm/Core.elm
+++ src/elm/Core.elm
@@ -1,7 +1,13 @@
 module Core exposing (..)
 
-type alias Position = (Int Int)
+import Dict
+import List
+import Maybe
+
 type alias State = Bool
+type alias Position = (Int, Int)
+type alias Cell = (Position, State)
+type alias Board = Dict.Dict Position State
 
 rule : State -> Int -> State
 rule state aroundAlives =
@@ -10,3 +16,34 @@ rule state aroundAlives =
     (True, 3)  -> True
     (False, 3) -> True
     _          -> False
+
+range : Int -> List Int
+range count = List.range 0 (count - 1)
+
+initCell : Position -> State -> Cell
+initCell pos state = (pos, state)
+
+initRow : Int -> Int -> State -> List Cell
+initRow y count state =
+  range count
+    |> List.map (\x -> initCell (y, x) state)
+
+initBoard : Int -> Int -> State -> Board
+initBoard height width state =
+  range height
+    |> List.map (\y -> initRow y width state)
+    |> List.concat
+    |> Dict.fromList
+
+
+toggleCell : Board -> Position -> Board
+toggleCell board pos =
+  Dict.update pos (Maybe.andThen (\state -> Just (not state))) board
+
+height : Board -> Int
+height board =
+  Dict.foldl (\(y, _) _ i -> max y i) 0 board
+
+width : Board -> Int
+width board =
+  Dict.foldl (\(_, x) _ j -> max x j) 0 board
